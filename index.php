3f3e8578cbb8a400c2f0f1f2d6f968ac6df2f444
Vlad <samyside@yandex.ru>
2023-12-19 18:45:42 +0300

    New route for fetch ip-address

--- index.php
+++ index.php
@@ -6,4 +6,13 @@ app()->get('/', function () {
   response()->json(['message' => 'Hello World!']);
 });
 
+app()->get('/catalog', function() {
+  echo '<h1>This is catalog</h1>';
+});
+
+app()->post('/ip', function() {
+  $ip_address = file_get_contents('http://ifconfig.me/ip');
+  echo "Your external IP: $ip_address";
+});
+
 app()->run();
